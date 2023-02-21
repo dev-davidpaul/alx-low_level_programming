6ec2acbdaeab0b0fff3a16caab11fa3e6c5190f1
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-02-20 17:16:46 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = '9';
+	while
+		(d < 10) {
+			putchar(d + 'a');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
