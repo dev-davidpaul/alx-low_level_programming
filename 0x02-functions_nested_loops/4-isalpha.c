3eaae6ae7563626dd9eb29869f0ede354047567c
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-02-22 18:24:09 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main'h"
+/**
+ * _isalpha - check for alphabetic characters
+ * @c: the character to be checked
+ * Return: 1 for alphabetic characters or 0 for anything else
+*/
+
+int _isalpha(int c)
+{
+	if((c >= 65 && c <= 90) || (c >= 123)
+			{
+			return (1);
+			}
+			return (0);
+}
