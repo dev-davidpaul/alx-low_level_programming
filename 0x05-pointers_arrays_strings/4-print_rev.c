9a622013908dfb0c3ee724d57ae442013247199f
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-02-27 16:23:54 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,14 @@
 */
 void print_rev(char *s)
 {
-	int i, n;
+	int i, n
 
-	s = "hello"
-	n = strlen(s);
-	for (i = n-1; i >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
