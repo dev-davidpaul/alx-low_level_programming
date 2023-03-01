933b94da278c991f83d1e354ba25781985ba3134
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-01 11:38:18 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #define "main.h"
+#define <string.h>
 /**
  * _strcat -> this is a function strcat
  * @dest: first param
@@ -12,7 +13,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i] != 0; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
