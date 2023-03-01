5fd1d0afde79c3e08eb93b8e68088c6e3a5d8ae3
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-01 07:27:37 -0800

    1-strncat.c

--- 1-strncat.c
+++ 1-strncat.c
@@ -1,6 +1,13 @@
 #include "main.h"
 /**
- *
+ * _strncat -> function to append some charx
+ * @dest: first param
+ * @src: second param
+ * @n: thrid param
+ * Return: string
 */
 char *_strncat(char *dest, char *src, int n);
 {
+	strncat(*dest, *src, n);
+	return (dest);
+}
