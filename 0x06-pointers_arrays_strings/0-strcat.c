5e43e200aa3f217a2787384163b93af2f8f61920
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-01 11:32:09 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#define "main.h"
+/**
+ * _strcat -> this is a function strcat
+ * @dest: first param
+ * @src: second param
+ * Return: a string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, 1;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
