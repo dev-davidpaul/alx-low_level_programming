76a50acc349883e0579dd13bcdc30a3187bd5b43
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-09 17:18:29 -0800

    new

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,7 +37,7 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
-			return (0);
+		return (0);
 	if (i >= len)
 		return (1);
 	return (check_pal(s, i + 1, len - 1));
