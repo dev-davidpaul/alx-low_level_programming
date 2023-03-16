86dd70b9a4e01a7c5559dc7803eb099923e93efb
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-15 18:34:20 -0700

    start

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr -> string chracter
+ * @s: string given
+ * @c: another char
+ * Return: a string
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0';i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
+}
