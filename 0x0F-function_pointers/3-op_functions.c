b59d1292976f026f4d1700e539cb36e4df3713c3
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-03-23 19:37:38 -0700

    now

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_ad - returns the sum of two numbers
+ * op_add - returns the sum of two numbers
  * @a: the first number
  * @b: the second number
  * Return: the sum of a and b
@@ -25,7 +25,7 @@ int op_add(int a, int b)
 */
 int op_sub(int a, int b)
 {
-	return (a- b);
+	return (a - b);
 }
 
 /**
