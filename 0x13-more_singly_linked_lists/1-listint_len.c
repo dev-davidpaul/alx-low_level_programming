5bf58622f932b621bc625380fab5daec11a58c33
dev-davidpaul <dev.davidpaul1@gmail.com>
2023-04-04 18:54:31 -0700

    start

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * listint_len - returns the number of element
+  * in a liquid listint_ list
+  * @h: a pointer to the head of the listint_t list
+  * Return: the number of pointer in the listint_t list
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * get_nodeint_at_index - location a given node of
+  * a listint_t linked list
+  * @head: a pointer to the head of the listint_t list
+  * @index: the index of the node to locate - indicates start at 0
+  * Return: if the node does not exits - NULL
+  * otherwise - the located node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
